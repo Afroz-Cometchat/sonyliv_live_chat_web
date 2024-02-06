104e228ea22fd23d29396531e37289949b82bb8f
Afroz.Cometchat <afroz.khan@cometchat.com>
2024-02-07 02:16:23 +0530

    updated transientmessage message received

--- src/components/GroupsList/Views/LiveReaction.jsx
+++ src/components/GroupsList/Views/LiveReaction.jsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 
 export const CometChatLiveReactionView = memo((props) => (
     props.showReactions
-        ? <cometchat-live-reaction reactinIconURL="https://emojiisland.com/cdn/shop/products/Emoji_Icon_-_Sunglasses_cool_emoji_large.png?v=1571606093" />
+        ? <cometchat-live-reaction reactinIconURL={props.reactionURL} />
         : null
 ));
 
--- src/components/GroupsList/index.jsx
+++ src/components/GroupsList/index.jsx
@@ -1,12 +1,13 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './style.css'
-import { CometChatGroups, CometChatLiveReaction, CometChatMessageEvents, GroupsStyle } from '@cometchat/chat-uikit-react';
+import { CometChatGroups, CometChatLiveReaction, CometChatMessageEvents, CometChatUIKit, GroupsStyle } from '@cometchat/chat-uikit-react';
 import { CometChat, MessagesRequest, MessagesRequestBuilder } from '@cometchat/chat-sdk-javascript';
 import MessageList from '../MessageList/index';
 import solylivhearticon from '../../assets/images/solylivhearticon.png'
 import ListItemView from './Views/ListItemView';
 import { CometChatLiveReactionView } from './Views/LiveReaction';
 import ExtensionPollBubble from '../ExtensionPollBubble';
+import fireHeart from '../../assets/images/fireHeart.png'
 // import CometChatLiveReactionView from './Views/LiveReaction';
 
 function GroupsList(props) {
@@ -19,9 +20,18 @@ function GroupsList(props) {
     // handle animated live reactions [under progress]
     const [showReactions, setShowReactions] = useState(false);
 
+    // handle show more live reaction options or not
+    const [showLiveReactionOptions, setShowLiveReactionOptions] = useState(false);
+
     // set poll to render at the top
     const [groupPoll, setGroupPoll] = useState(false);
     const [triggerGrouPoll, setTriggerGrouPoll] = useState(false);
+
+    // to detect single or double click on live emoji reaction
+    const timer = useRef(null);
+
+    // set live reaction icon url
+    const [reactionURL, setReactionURL] = useState('');
 
     // set custom group list view
     const listItemView = (group) => {
@@ -56,9 +66,22 @@ function GroupsList(props) {
 
     // send live reaction [under progress]
     const sendLiveReaction = useCallback(() => {
-        // console.log("start");
-        let receiverId = "supergroup";
+        if (timer.current !== null) return;
+        let receiverId = "100thieves";
         let receiverType = CometChat.RECEIVER_TYPE.GROUP;
+        timer.current = setTimeout(() => {
+            setShowReactions(true)
+            const data = { type: "reaction", reactionURL: "https://emojiisland.com/cdn/shop/products/Emoji_Icon_-_Sunglasses_cool_emoji_large.png?v=1571606093" };
+            CometChat.getLoggedinUser().then((user) => {
+                let transientMessage = new CometChat.TransientMessage(receiverId, receiverType, data).setSender(user)
+                CometChat.sendTransientMessage(transientMessage);
+            })
+            setTimeout(() => {
+                setShowReactions(false)
+            }, 2500);
+            timer.current = null;
+        }, 250);
+        // console.log("start");
         // let data = { "LIVE_REACTION": "heart" };
 
         // let transientMessage = new CometChat.TransientMessage(receiverId, receiverType, data);
@@ -71,10 +94,6 @@ function GroupsList(props) {
         // CometChatMessageEvents.ccLiveReaction.next(reactionURL);
         // console.log("end");
         // setReactionIconURL("https://emojiisland.com/cdn/shop/products/Emoji_Icon_-_Sunglasses_cool_emoji_large.png?v=1571606093")
-        setShowReactions(true)
-        setTimeout(() => {
-            setShowReactions(false)
-        }, 2500);
     }, [])
 
     useEffect(() => {
@@ -85,6 +104,7 @@ function GroupsList(props) {
             new CometChat.MessageListener({
                 onTransientMessageReceived: transientMessage => {
                     console.log('transient message received', transientMessage);
+                    setReactionURL(transientMessage.data.reactionURL)
                     setShowReactions(true)
                     setTimeout(() => {
                         setShowReactions(false)
@@ -135,6 +155,7 @@ function GroupsList(props) {
             new CometChat.MessageListener({
                 onTransientMessageReceived: transientMessage => {
                     console.log('transient message received', transientMessage);
+                    setReactionURL("https://i.pinimg.com/474x/fc/57/8b/fc578b392d04c6885249f36413da7cbf.jpg")
                     setShowReactions(true)
                     setTimeout(() => {
                         setShowReactions(false)
@@ -149,7 +170,23 @@ function GroupsList(props) {
     }, [triggerGrouPoll])
 
     // [ under progress ]
-    const memoizedChild = useMemo(() => <CometChatLiveReactionView showReactions={showReactions} />, [showReactions]);
+    // const memoizedChild = useMemo(() => <CometChatLiveReactionView showReactions={showReactions} reactionURL={reactionURL} />, [showReactions]);
+
+    // handle live event double click
+    const handleDoubleClick = () => {
+        clearTimeout(timer.current);
+        timer.current = null;
+        setShowLiveReactionOptions(true);
+    }
+
+    // handle other live reactions click
+    const sendOtherLiveReaction = (reaction) => {
+        setShowReactions(true)
+        setTimeout(() => {
+            setShowReactions(false)
+        }, 2500);
+        setShowLiveReactionOptions(false)
+    }
 
     return (
         <div className='groupsListContainer'>
@@ -169,11 +206,32 @@ function GroupsList(props) {
                             <CometChatGroups {...cometChatGroupProps} />
                             <div>
                                 {/* {showReactions && <CometChatLiveReactionView />} */}
-                                {memoizedChild}
+                                {/* {memoizedChild} */}
+                                <CometChatLiveReactionView showReactions={showReactions} reactionURL={'https://i.pinimg.com/474x/fc/57/8b/fc578b392d04c6885249f36413da7cbf.jpg'} />
                             </div>
                         </div>
                 }
-                {joinedGroup ? <></> : <div><img src={solylivhearticon} className='grouplist_heart_icon' alt="" onClick={() => sendLiveReaction()} /></div>}
+                {joinedGroup ? <></> :
+                    <div className='live_reaction_container'>
+                        {showLiveReactionOptions && <div className='other_live_reaction_container'>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128525;</span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128545;</span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128518;</span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128559;</span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}><img src={fireHeart} alt="" /></span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128077;</span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128546;</span>
+                            <span className="live_reaction_emoji" onClick={() => sendOtherLiveReaction("ji")}>&#128293;</span>
+                        </div>}
+                        <span className='live_reaction_emoji'>
+                            <img
+                                src={solylivhearticon}
+                                alt=""
+                                onClick={() => sendLiveReaction()}
+                                className='main_live_emoji'
+                                onDoubleClick={handleDoubleClick} />
+                        </span>
+                    </div>}
             </div>
         </div>
     )

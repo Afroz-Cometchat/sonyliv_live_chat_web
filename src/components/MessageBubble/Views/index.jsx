eff714d3f17200925b0077158c8fd3a58a872866
Afroz.Cometchat <afroz.khan@cometchat.com>
2024-02-08 01:58:26 +0530

    updated list syles

--- src/components/MessageBubble/Views/index.jsx
+++ src/components/MessageBubble/Views/index.jsx
@@ -19,7 +19,7 @@ const MessageBubbleReactionsView = ({ message, setIsLiked, setIsMyReaction }) =>
                             setIsMyReaction(true);
                             myReactionFlag = false
                         }
-                    }else{
+                    } else {
                         if (myCount.includes(user.name)) {
                             setIsLiked(true)
                             flag = false;
@@ -54,66 +54,3 @@ const MessageBubbleReactionsView = ({ message, setIsLiked, setIsMyReaction }) =>
 }
 
 export { MessageBubbleReactionsView };
-
-
-
-
-// import { CometChatUIKit } from "@cometchat/chat-uikit-react"
-// import { useEffect, useState } from "react"
-
-// const MessageBubbleReactionsView = ({ message, setIsLiked, setIsMyReaction }) => {
-//     const [reactionsCount, setReactionsCount] = useState(false);
-//     const [reactionsData, setReactionsData] = useState([]);
-
-//     useEffect(() => {
-//         const handleReactionData = async () => {
-//             const user = await CometChatUIKit.getLoggedinUser();
-//             const reactions = message.metadata?.['@injected']?.extensions?.reactions;
-
-//             if (reactions) {
-//                 let reactionsDataTemp = Object.keys(reactions);
-//                 let count = 0;
-//                 let myReactionFlag = false;
-
-//                 reactionsDataTemp.forEach((reactionKey) => {
-//                     const reactionUsers = Object.keys(reactions[reactionKey]);
-//                     if (reactionKey !== '🛵') {
-//                         count += reactionUsers.length;
-//                         if (reactionUsers.includes(user.name)) {
-//                             myReactionFlag = true;
-//                         }
-//                     } else if (reactionUsers.includes(user.name)) {
-//                         setIsLiked(true);
-//                     } else {
-//                         setIsLiked(false);
-//                     }
-//                 });
-
-//                 setIsMyReaction(myReactionFlag);
-//                 setReactionsCount(count);
-//                 setReactionsData(reactionsDataTemp.filter(ele => ele !== '🛵'));
-//             } else {
-//                 setIsLiked(false);
-//                 setIsMyReaction(false);
-//             }
-//         };
-
-//         handleReactionData();
-//     }, [message]);
-
-//     return (
-//         <>
-//             {
-//                 reactionsCount > 0 ?
-//                     <div className='reactionEmojiContainer'>
-//                         <div className='reactionsEmojis'>
-//                             {reactionsData.map((ele) => <span>{ele}</span>)}
-//                         </div>
-//                         <span className='reactionsCount'>{reactionsCount}</span>
-//                     </div> : null
-//             }
-//         </>
-//     )
-// }
-
-// export { MessageBubbleReactionsView };
